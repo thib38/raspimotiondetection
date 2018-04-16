a71b8587479dc32e1cb0521b3888b803cba659d9
thib38 <thibault.enipel@gmail.com>
2018-04-16 13:39:35 +0200

    INITIAL COMMIT

--- testraspberrypi/main.py
+++ testraspberrypi/main.py
@@ -1,9 +1,21 @@
-from  picamera import PiCamera
-from time import sleep
 import sys
+from time import sleep
+from  picamera import PiCamera
+import cv2
+import imutils
 print(sys.version_info)
 
+
+CAMERA_WARMUP_TIME = 2.5  # seconds
+RESOLUTION = [640,400]
+FPS = 16
+MIN_AREA = 5000
+
+
 camera = PiCamera()
+
+
+
 camera.start_preview()
 for _ in range(10):
     sleep(5)
@@ -12,7 +24,6 @@ for _ in range(10):
 
 camera.stop_preview()
 
-# gfdhqk ghqddkg hk
 
 
 
