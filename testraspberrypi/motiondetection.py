666648223ddedb7a85a33b5bae27972ceab7b72a
thib38 <thibault.enipel@gmail.com>
2018-04-30 17:26:10 +0200

    create class for camera capture

--- testraspberrypi/motiondetection.py
+++ testraspberrypi/motiondetection.py
@@ -140,9 +140,12 @@ class GetFrameFromCamera:
 
     def loop_forever_get_frame(self):
 
+
         with PiCamera() as camera:
+            camera.resolution = self.resolution
+            camera.framerate = self.fps
             # rawCapture = PiRGBArray(camera, size=self.resolution)
-            rawCapture = PiRGBArray(camera, size=(640,480))
+            rawCapture = PiRGBArray(camera, size=self.resolution)
 
             # allow the camera to warmup, then initialize the average frame, last
             # uploaded timestamp, and frame motion counter
@@ -272,87 +275,86 @@ if __name__ == "__main__":
     send_over_lan = SendPictureToCentral(CAMERA_ID)
 
     # set camera parameters
-    # camera_handler = GetFrameFromCamera(RESOLUTION,
-    #                                     FPS,
-    #                                     MIN_AREA,
-    #                                     DELTA_THRESHOLD,
-    #                                     CAMERA_ID)
+    camera_handler = GetFrameFromCamera(RESOLUTION,
+                                        FPS,
+                                        MIN_AREA,
+                                        DELTA_THRESHOLD,
+                                        CAMERA_ID)
+
+    # start frame acquisition loop
+    camera_handler.loop_forever_get_frame()
+
+    # with PiCamera() as camera:
+    #     camera.resolution = RESOLUTION
+    #     camera.framerate = FPS
+    #     rawCapture = PiRGBArray(camera, size=RESOLUTION)
     #
-    # # start frame acquisition loop
-    # camera_handler.loop_forever_get_frame()
-
-    camera = PiCamera()
-    with PiCamera() as camera:
-        camera.resolution = RESOLUTION
-        camera.framerate = FPS
-        rawCapture = PiRGBArray(camera, size=RESOLUTION)
-
-        # allow the camera to warmup, then initialize the average frame, last
-        # uploaded timestamp, and frame motion counter
-        print("[INFO] warming up...")
-        sleep(CAMERA_WARMUP_TIME)
-        avg = None
-        lastUploaded = datetime.datetime.now()
-        motionCounter = 0
-
-        # capture frames from the camera
-        for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            print("frame captured")
-            # grab the raw NumPy array representing the image and initialize
-            # the timestamp and occupied/unoccupied text
-            frame = f.array
-            timestamp = datetime.datetime.now()
-            # text = "Unoccupied"
-
-            # resize the frame, convert it to grayscale, and blur it
-            frame = imutils.resize(frame, width=500)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-            # if the average frame is None, initialize it
-            if avg is None:
-                print("[INFO] starting background model...")
-                avg = gray.copy().astype("float")
-                rawCapture.truncate(0)  # clear buffer before next iteration
-                continue
-
-            # accumulate the weighted average between the current frame and
-            # previous frames, then compute the difference between the current
-            # frame and running average
-            cv2.accumulateWeighted(gray, avg, 0.5)
-            frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-            # threshold the delta image, dilate the thresholded image to fill
-            # in holes, then find contours on thresholded image
-            thresh = cv2.threshold(frameDelta, DELTA_THRESHOLD, 255,
-                                   cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
-            cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-            # loop over the contours
-            for c in cnts:
-                # if the contour is too small, ignore it
-                if cv2.contourArea(c) < MIN_AREA:
-                    continue
-
-                # compute the bounding box for the contour, draw it on the frame,
-                # and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                # text = "Occupied"
-                print("=======MOTION DETECTED============")
-
-                # draw the text and timestamp on the frame
-                ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-                # cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                # cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                #             0.35, (0, 0, 255), 1)
-                cv2.putText(frame, ts, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.35, (0, 0, 255), 1)
-
-                handle_frame(frame, ts.replace(" ","_").replace(":","_"))
-
-            rawCapture.truncate(0)  # clear buffer before next iteration
+    #     # allow the camera to warmup, then initialize the average frame, last
+    #     # uploaded timestamp, and frame motion counter
+    #     print("[INFO] warming up...")
+    #     sleep(CAMERA_WARMUP_TIME)
+    #     avg = None
+    #     lastUploaded = datetime.datetime.now()
+    #     motionCounter = 0
+    #
+    #     # capture frames from the camera
+    #     for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    #         print("frame captured")
+    #         # grab the raw NumPy array representing the image and initialize
+    #         # the timestamp and occupied/unoccupied text
+    #         frame = f.array
+    #         timestamp = datetime.datetime.now()
+    #         # text = "Unoccupied"
+    #
+    #         # resize the frame, convert it to grayscale, and blur it
+    #         frame = imutils.resize(frame, width=500)
+    #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #         gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    #
+    #         # if the average frame is None, initialize it
+    #         if avg is None:
+    #             print("[INFO] starting background model...")
+    #             avg = gray.copy().astype("float")
+    #             rawCapture.truncate(0)  # clear buffer before next iteration
+    #             continue
+    #
+    #         # accumulate the weighted average between the current frame and
+    #         # previous frames, then compute the difference between the current
+    #         # frame and running average
+    #         cv2.accumulateWeighted(gray, avg, 0.5)
+    #         frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    #
+    #         # threshold the delta image, dilate the thresholded image to fill
+    #         # in holes, then find contours on thresholded image
+    #         thresh = cv2.threshold(frameDelta, DELTA_THRESHOLD, 255,
+    #                                cv2.THRESH_BINARY)[1]
+    #         thresh = cv2.dilate(thresh, None, iterations=2)
+    #         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    #                                 cv2.CHAIN_APPROX_SIMPLE)
+    #         cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    #
+    #         # loop over the contours
+    #         for c in cnts:
+    #             # if the contour is too small, ignore it
+    #             if cv2.contourArea(c) < MIN_AREA:
+    #                 continue
+    #
+    #             # compute the bounding box for the contour, draw it on the frame,
+    #             # and update the text
+    #             (x, y, w, h) = cv2.boundingRect(c)
+    #             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #             # text = "Occupied"
+    #             print("=======MOTION DETECTED============")
+    #
+    #             # draw the text and timestamp on the frame
+    #             ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    #             # cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+    #             #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    #             # cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+    #             #             0.35, (0, 0, 255), 1)
+    #             cv2.putText(frame, ts, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+    #                         0.35, (0, 0, 255), 1)
+    #
+    #             handle_frame(frame, ts.replace(" ","_").replace(":","_"))
+    #
+    #         rawCapture.truncate(0)  # clear buffer before next iteration
